a5f80e41f18d9bd3e739ddea68d041f0976e19fa
Ritik Kumar <89124624+ritikkumar85@users.noreply.github.com>
2023-09-20 22:16:09 +0530

    Add files via upload

---
+++ Basic Banking App/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
+
+void main() {
+  runApp(MaterialApp(
+    themeMode: ThemeMode.dark,
+    debugShowCheckedModeBanner: false,
+  ));
+}
